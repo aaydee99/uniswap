6b1d77df44aacdaa79701b85dbe18286c9310c6f
Abdullah Akhtar <44116978+aaydee99@users.noreply.github.com>
2022-04-05 22:07:42 +0500

    commit

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,17 +1,13 @@
-require("@nomiclabs/hardhat-waffle");
-const fs = require("fs")
+require('@nomiclabs/hardhat-waffle')
 
-
-const privateKey = fs.readFileSync(".secret").toString();
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: "https://eth-rinkeby.alchemyapi.io/v2/x0rXCEwfvKRJBZRnttReQ0VVcA491oFl",
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/mEJIAuxHnE2HsvBATUIEBiTLrd-dN5X3',
       accounts: [
-        privateKey
-      ]
-
+        'f2679263aadf9db7949246a5265f7cf0b0aece610ba4d84dcfc9304ccd050d0c',
+      ],
     },
   },
-};
+}
--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,26 +1,18 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
-
 const main = async () => {
-  const transactionFactory = await hre.ethers.getContractFactory('Transactions');
-  const transactionContract = await transactionFactory.deploy();
+  const transactionFactory = await hre.ethers.getContractFactory('Transactions')
+  const transactionContract = await transactionFactory.deploy()
 
-  await transactionContract.deployed();
+  await transactionContract.deployed()
 
-  console.log("Transactions deployed to:", transactionContract.address);
+  console.log('Transactions deployed to:', transactionContract.address)
 }
 
-
-;(async() => {
-  try{
+;(async () => {
+  try {
     await main()
     process.exit(0)
-  }catch(error){
-    console.error(error);
-    process.exit(1);
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
   }
 })()
